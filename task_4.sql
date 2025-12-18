5c513b3c74a5df179a55c2598c512f925f41822a
nancymwende <nancymwende860@gmail.com>
2025-12-18 23:10:34 +0300

    Update task_4.sql to use INFORMATION_SCHEMA.COLUMNS

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,13 @@
--- Use the alx_book_store database
-USE alx_book_store;
-
--- Show full description of the books table
-SHOW COLUMNS FROM Books;
+-- Select full description of the books table from INFORMATION_SCHEMA
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
